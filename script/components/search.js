20cfc00e18ea0e12d5b7bd0aec9f34be8e82ea89
Rémy Merienne <remymerienne@hotmail.fr>
2021-11-25 19:01:22 +0100

    [FEAT]: add sort by ingredients

--- script/components/search.js
+++ script/components/search.js
@@ -1,50 +1,89 @@
-import {
-  recipes
-} from '../../recipes';
+import { recipes } from '../../recipes';
+
+// = Nettoyage console pour test
+const clearConsole = () => {  
+const uiNodeSearchBar = document.querySelector('.search-bar__input');
+  uiNodeSearchBar.addEventListener('keydown', () => {
+    console.clear();
+    console.log(recipes);
+  });
+};
+
+const formatString = (string) => {
+  string = string.toLowerCase();
+  string = string.replace(/[éèëê]/g, 'e');
+  string = string.replace(/[îï]/g, 'i');
+  string = string.replace(/[à]/g, 'a');
+  string = string.replace(/[ç]/g, 'c');
+  return string;
+};
 
 export const search = () => {
 
-  console.log(recipes);
+  clearConsole();
 
   const uiNodeSearchBar = document.querySelector('.search-bar__input');
 
   uiNodeSearchBar.addEventListener('keyup', e => {
 
+    // - Lancement de la recherche au troisième caractère saisi
     if (e.target.value.length >= 3) {
 
-      let recipeId = 1;
+      let i = 0;
       let recipesFound = [];
 
-      while (recipeId != (recipes.length + 1)) {
+      // - Boucle principale inspectant toutes les recettes Id par Id
+      while (i < recipes.length) {
 
-        const userSearch = e.target.value.toLowerCase();
-        const recipeTested = recipes.filter(p => p.id === recipeId);
+        const userSearch = formatString(e.target.value);
+        const recipeTested = recipes[i];
 
-        const recipeTestedByName = recipeTested[0].name.toLowerCase();
-        const resultName = recipeTestedByName.indexOf(userSearch);
+        // - Comparaison de la saisi avec le nom de la recette
+        const recipeName = formatString(recipeTested.name);
+        const testResultByName = recipeName.indexOf(userSearch);
 
-        const recipeTestedByDescription = recipeTested[0].description.toLowerCase();
-        const resultDescription = recipeTestedByDescription.indexOf(userSearch);
-
-        /*
-        const chose = recipeTested[0].ingredients;
-        for (let i in chose) {
-          console.log(chose[i].ingredient);
+        // = Affichage console pour test ***
+        if (testResultByName !== -1) {
+          console.log(`Recette N° ${recipeTested.id} - Nom : ${recipeName}`);
         }
-        */
+        // = Affichage console pour test-END ***
 
-        if (resultName !== -1 || resultDescription !== -1) {
+        // - Comparaison de la saisi avec la description de la recette
+        const recipeDescription = formatString(recipeTested.description);
+        const testResultByDescription = recipeDescription.indexOf(userSearch);
 
-          recipesFound.push(recipeId);
+        // - Comparaison de la saisi avec les ingrédients de la recette-START ***
+        const ingredients = recipeTested.ingredients;
+        let testResultByIngredient;
+
+        for (let i in ingredients) {
+
+          const recipeIngredient = formatString(ingredients[i].ingredient);
+          testResultByIngredient = recipeIngredient.indexOf(userSearch);
+
+          if (testResultByIngredient !== -1) {
+            // = Affichage console pour test ***
+            console.log(`Recette N° ${recipeTested.id} - Ingrédient : ${recipeIngredient}`);
+            // = Affichage console pour test-END ***
+            break;
+          }
+
+        }
+        // - Comparaison de la saisi avec les ingrédients de la recette-END ***
+
+        // - Test de correspondance générale
+        if (testResultByName !== -1 || testResultByDescription !== -1 || testResultByIngredient !== -1) {
+
+          // - Tableau contenant les Id des recettes ayant correspondance avec la saisie utilisateur
+          recipesFound.push(recipeTested.id);
 
         }
 
-        recipeId++;
+        i++;
 
       }
 
       console.log(recipesFound);
-
     }
 
   });
